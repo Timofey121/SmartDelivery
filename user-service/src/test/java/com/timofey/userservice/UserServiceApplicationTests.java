e1e63f7b41e1f245229ea83534eeef70b3746345
timofey <ale3jurtaev@gmail.com>
2025-04-18 01:33:25 +0300

    Add Eureka service discovery server

--- user-service/src/test/java/com/timofey/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/timofey/userservice/UserServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.timofey.eurekaservice;
+package com.timofey.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServiceApplicationTests {
+class UserServiceApplicationTests {
 
     @Test
     void contextLoads() {
