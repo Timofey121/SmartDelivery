0a8d0237a1229e8ce7527429113c8d00b6ad1d9e
timofey <ale3jurtaev@gmail.com>
2025-04-17 22:37:35 +0300

    Add authentication service with JWT implementation

---
+++ auth-service/src/main/java/com/timofey/authservice/dto/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.timofey.authservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    private String username;
+    private String password;
+}
